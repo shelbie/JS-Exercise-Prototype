894c7a75ef87cbe8e7c8603cfe97682928ad3b59
Shelbie <30550643+shelbie@users.noreply.github.com>
2020-03-17 19:19:38 -0500

    finished baby consttructor challenge

--- index.js
+++ index.js
@@ -101,7 +101,25 @@ Car.prototype.drive = function(distance) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {}
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+this.favoriteToy = favoriteToy;
+
+Person.call(this, name, age);
+}
+
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+}
+
+
+
+
+
+
+
 
 /* 
   TASK 4
